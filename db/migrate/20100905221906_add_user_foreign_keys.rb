5cae53aabbf1f6b73be89ceadab589c286438a39
Rodney Degracia <rdegraci@gmail.com>
2010-09-15 16:22:48 -0400

    no need to add foreign key

--- db/migrate/20100905221906_add_user_foreign_keys.rb
+++
@@ -1,9 +0,0 @@
-class AddUserForeignKeys < ActiveRecord::Migration
-  def self.up
-    add_column :roles, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :roles, :user_id
-  end
-end
