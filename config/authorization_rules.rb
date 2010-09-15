a0abda3d7cf4cf18e4d7ef9f25e74c4deb468764
Rodney Degracia <rdegraci@gmail.com>
2010-09-15 16:22:27 -0400

    bug fix
    Given admin needed access

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -2,6 +2,7 @@ authorization do
   role :admin do
     has_permission_on [:users ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
     has_permission_on [:user_sessions ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    
     has_permission_on [:simcharacters ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
     has_permission_on [:simdoors ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
     has_permission_on [:simdowns ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
@@ -15,6 +16,11 @@ authorization do
     has_permission_on [:simthings ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
     has_permission_on [:simups ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
     has_permission_on [:simwests ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    
+    has_permission_on [:sim_modules ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    has_permission_on [:sim_klasses ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    has_permission_on [:sim_variables ], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    has_permission_on [:sim_scripts ], :to =>[:index, :show, :new, :create, :edit, :update, :destroy]
     
   end
 
